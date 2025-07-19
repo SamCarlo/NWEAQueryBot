ac3cbe9934cb8df85ef6f31ec72df2458e4d58f6
Sam Carlos <samuelryancarlos@gmail.com>
2025-07-18 16:13:48 -1000

    added config file to  whole repo

---
+++ config.py
@@ -0,0 +1,16 @@
+# Main (private) database path
+priv_db_path = "private.db"
+
+# Model (anonymized, public) database path
+anon_db_path = "anon.db"
+
+# Paths for 2024-2025 data
+sp25_results_path = './data/spring24-25/AssessmentResults.csv'
+sp25_teachers_path = './data/spring24-25/ClassAssignments.csv'
+sp25_students_path = './data/spring24-25/StudentsBySchool.csv'
+
+# Descriptive CSV's to explain columns to the genai
+results_metadata = './data/spring24-25/results_metadata.csv'
+teachers_metadata = './data/spring24-25/teachers_metadata.csv'
+students_metadata = './data/spring24-25/students_metadata.csv'
+
