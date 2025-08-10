2bf1cf985c168ff21fcd5e9e9b59f53dd4a70e7a
Sam Carlos <samuelryancarlos@gmail.com>
2025-08-10 10:13:15 -1000

    Added NWEA Data Agent project files as they currently stand. Will create branches and changes from this version.

---
+++ config.py
@@ -0,0 +1,11 @@
+# Main (private) database path
+priv_db_path = "private.db"
+
+# Model (anonymized, public) database path
+anon_db_path = "anon.db"
+
+# Folders
+fall_folder = "./data/fall24-25"
+spring_folder = "./data/spring24-25"
+
+
